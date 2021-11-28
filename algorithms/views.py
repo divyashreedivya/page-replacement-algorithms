a38bda2b3b8343c2181ef88267a1d10a3cf0557b
bobettes1 <kenzelkdsouza@gmail.com>
2021-11-28 19:09:25 +0530

    Added lru code

--- algorithms/views.py
+++ algorithms/views.py
@@ -55,7 +55,41 @@ def fifo(sequence,frameAmt):
     return fifoctx
 
 #lru alogorithm
-def lru(sequence,frameAmt):
+
+
+def lru(sequence, frameAmt):
+    lrufinalList[]
+    sequenceList = sequence
+    frames = []
+    lruallList = []
+    hit = 0
+    miss = 0
+    temp = []
+
+    #lru algorithm
+    for s in sequenceList:
+        if s not in frames:
+            miss += 1
+            if len(frames) == frameAmt:
+                frames.remove(frames[0])
+                frames.append(s)
+                temp = frames[:]
+                temp[-1] = 'red'+temp[-1]
+            else:
+                frames.append(s)
+                temp = frames[:]
+                temp[-1] = 'red'+temp[-1]
+        else:
+            hit += 1
+            frames.remove(s)
+            frames.append(s)
+            temp = frames[:]
+
+        lruallList.append(temp)
+    lrufinalList = transpose(lruallList, frameAmt)#transpose
+    
+    lruratio = 100.0*hit/(len(sequenceList))
+    lructx = {'finalList': lrufinalList, 'miss': miss,'hit': hit, 'ratio': ratio}  # fifo object
     pass
 
 #opt algorithm
